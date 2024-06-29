cee3c9b7abe51bd765a2cdc20d0b6b82b3705d16
Kenny Marshall <kenny.mrshll@gmail.com>
2024-06-29 14:42:59 +0200

    use comments in example file

--- argParseExample.cpp
+++ argParseExample.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 int main(int argc, char *argv[]){
     ArgParser ap;
-    ap.addPositional("first", "1");
+    ap.addPositional("first", "1"); //args - long name, default value (string, default: "")
     ap.addPositional("second","2");
-    ap.addKW("firstkw","3", "fkw");
+    ap.addKW("firstkw","3", "fkw"); // args - long name, default value (string, default: ""), short name
     ap.addKW("secondkw","4");
     ap.readArguments(argc,argv);
-    map<string,string> allArgs = ap.getAllArgs();
+    map<string,string> allArgs = ap.getAllArgs(); //returns map of all arguments with long names as strings
 
     cout << "first positional: " << allArgs["first"] << endl;
     cout << "second positional: " << allArgs["second"] << endl;
     cout << "firstkw: " << allArgs["firstkw"] << endl;
     cout << "secondkw: " << allArgs["secondkw"] << endl;
-    int firstkw = ap.getArg<int>("firstkw");
+    int firstkw = ap.getArg<int>("firstkw"); // get individual arguments as int, float or string types
     float firstPos = ap.getArg<float>("first");
     string secondPos = ap.getArg<string>("second");
     cout << firstkw << endl;
